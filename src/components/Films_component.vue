ff78180050ae60386e26b138d3cf15c9a756d31a
Simon Balleux <simoncestmoi@hotmail.fr>
2022-06-27 10:56:20 +0200

    création page selection film

--- src/components/Films_component.vue
+++ src/components/Films_component.vue
@@ -1,6 +1,47 @@
 <template>
   <div>
-    <h1>Je test ma page selection film</h1>
+    <div class="row">
+    <!-- Tableau -->
+    <div class="table-responsive mb-5 mt-5 pr-5 pl-5">
+      <table class="table table-striped table-dark">
+        <!-- Header Tableau -->
+        <thead>
+          <tr>
+            <th scope="col">Date</th>
+            <th scope="col">Film</th>
+            <th scope="col">Horaire</th>
+            <th scope="col">Detail Film</th>
+          </tr>
+        </thead>
+        <!-- Body Tableau -->
+        <tbody>
+          <tr class="tabrow">
+            <th scope="row">05/08</th>
+            <td>Nom du film 1</td>
+            <td>De x a x</td>
+            <td><a href="#" class="btn btn-secondary">Details</a></td>
+          </tr>
+
+          <tr class="tabrow">
+            <th scope="row">05/08</th>
+            <td>Nom du film 2</td>
+            <td>De x a x</td>
+            <td><a href="#" class="btn btn-secondary">Details</a></td>
+          </tr>
+
+          <tr class="tabrow">
+            <th scope="row">05/08</th>
+            <td>Nom du film 3</td>
+            <td>De x a x</td>
+            <td><a href="#" class="btn btn-secondary">Details</a></td>
+          </tr>
+
+        </tbody>
+      </table>
+    </div>
+    </div>
+    
+    
   </div>
 </template>
 
